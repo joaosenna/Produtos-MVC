9bf4356ee64e01695af7dfca9fdb3c1441bd12b1
joaosenna <35225451+joaosenna@users.noreply.github.com>
2022-04-01 11:43:02 -0300

    aula 01.04.2022
    Atualização do Spring MVC, para a exibição de mensagens.

---
+++ produtos-mvc/Produtos-MVC-13bc0c057af8852aa10ab371c2b46650c41a3d85/12produtosmvc/produtos-mvc/src/main/java/br/com/fiap/model/ProdutoModel.java
@@ -0,0 +1,105 @@
+package br.com.fiap.model;
+
+import javax.validation.constraints.Size;
+
+public class ProdutoModel {
+
+	private long id;
+	private String nome;
+	private String sku;
+	private String descricao;
+	private double preco;
+	private String caracteristicas;
+
+	public ProdutoModel() {
+	}
+
+	public ProdutoModel(Long id, String nome, String sku, String descricao, double preco, String caracteristicas) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.sku = sku;
+		this.descricao = descricao;
+		this.preco = preco;
+		this.caracteristicas = caracteristicas;
+	}
+	
+	
+	@Size(min = 8, max = 8, message = "SKU deve conter 8 caracteres")
+	public String getSku() {
+		return sku;
+	}
+	
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	
+	@Size(min = 1, max = 200, message = "Descrição deve ter no mínimo 1 e no máximo 200 caracteres")
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao=descricao;
+	}
+	
+	@DecimalMin(value = "0.1", message = "Preço deve ser acima de 0.0")
+	public double getPreco() {
+		return preco;
+	}
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getCaracteristicas() {
+		return caracteristicas;
+	}
+
+	public void setCaracteristicas(String caracteristicas) {
+		this.caracteristicas = caracteristicas;
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ produtos-mvc/src/main/java/br/com/fiap/controller/ProdutoController.java
@@ -0,0 +1,78 @@
+package br.com.fiap.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.fiap.model.ProdutoModel;
+import br.com.fiap.repository.ProdutoRepository;
+
+@Controller
+public class ProdutoController {
+	
+	ProdutoRepository repository = ProdutoRepository.getInstance();
+
+	@RequestMapping(value = "/produto", method = RequestMethod.GET)
+	public String findAll(Model model) {
+
+		model.addAttribute("produtos", repository.findAll());
+		return "produtos";
+	}
+
+	@RequestMapping(value = "/produto/{id}", method = RequestMethod.GET)
+	public String findById(@PathVariable("id") long id, Model model) {
+		
+		model.addAttribute("produto", repository.findById(id));
+		return "produto-detalhe";
+	}
+	
+	@RequestMapping(value = "/produto/new", method = RequestMethod.GET)
+	public String openSave() {
+		return "produto-novo";
+	}
+	
+	@RequestMapping(value = "/produto/new", method = RequestMethod.POST)
+	public String save(@Valid ProdutoModel produtoModel, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+	if(bindingResult.hasErrors()) {
+	return "produto-novo";
+	}
+	repository.save(produtoModel);
+	redirectAttributes.addFlashAttribute("messages", "Produto cadastrado com sucesso!");
+	return "redirect:/produto";
+	}
+	
+	@RequestMapping(value = "produto/update/{id}", method = RequestMethod.GET)
+	public String openUpdate(@PathVariable("id") long id, Model model) {
+		
+		model.addAttribute("produto", repository.findById(id));
+		return "produto-editar";
+	}
+	
+	@RequestMapping(value = "/produto/update", method = RequestMethod.POST)
+	public String update(Model model, ProdutoModel produtoModel, RedirectAttributes redirectAttributes) {
+		
+		repository.update(produtoModel);
+		redirectAttributes.addFlashAttribute("mesages", "Produto atualizado com sucesso!");
+		
+		return "redirect:/produto";
+	}
+	
+	@RequestMapping(value = "produto/delete/{id}", method = RequestMethod.DELETE)
+	public String delete(@PathVariable("id") long id, Model model, RedirectAttributes redirectAttributes ) {
+		
+		repository.deleteById(id);
+		redirectAttributes.addFlashAttribute("mesages", "Produto excluído com sucesso!");
+		
+		return "redirect:/produto";
+	}
+	
+	
+	
+
+}
